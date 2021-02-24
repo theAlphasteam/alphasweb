b1e8ce89ef5381b4012a37ff547a335eb9a3644f
miracleonyenma <miracleficient@gmail.com>
2021-02-24 23:19:10 +0100

    feat: add transition to #app

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   overflow-x: hidden;
   min-height: 100vh;
+
+  *{
+    transition: all 0.32s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
 }
 </style>
