7aea2bbae1e77584c6a499166fe8a4033a20b9d1
miracleonyenma <miracleficient@gmail.com>
2021-02-24 17:26:32 +0100

    create site-nav component

---
+++ src/components/SiteNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav id="site-nav">
+    <router-link to="/about">
+      <button class="cta cta--main">Contact Us</button>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "SiteNav"
+};
+</script>
+
+<style lang="scss" scoped>
+    @import '@/scss/_utils.scss';
+    @import '@/scss/_layout.scss';
+
+</style>
