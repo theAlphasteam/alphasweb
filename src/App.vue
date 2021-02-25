e87bb1654861c69323b87a7198ba0cde9d0c51f8
miracleonyenma <miracleficient@gmail.com>
2021-02-25 12:17:34 +0100

    update && feat: add data-wa-trans attribute and styling for page <transition>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <site-header></site-header>
-  <router-view />
+  <transition>
+    <router-view />
+  </transition>
 </template>
 
 <script>
@@ -14,7 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '@/scss/main.scss';
+@import "@/scss/main.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,8 +26,38 @@ export default {
   min-height: 100vh;
   background: var(--bg-2);
 
-  *{
+  * {
     transition: all 0.32s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+}
+
+[data-wa-trans="wrapper"] {
+  @for $count from 0 through 5 {
+    [data-wa-trans="child"]:nth-child(#{$count}) {
+      transition-duration: 0.32s + $count / 4 !important;
+    }
+
+    [data-wa-dur="#{$count}"] {
+      transition-duration: 0.32s + $count !important;
+    }
+  }
+}
+
+.v-enter-active {
+  transition-duration: 1s !important;
+}
+.v-leave-active {
+  transition-duration: 2s !important;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+
+  [data-wa-trans="child"],
+  [data-wa-dur] {
+    opacity: 0;
+    transform: translateY(100px);
   }
 }
 </style>
