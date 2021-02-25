2c87ad7dc21f1985beb4ef20c03d993bc7ebd094
miracleonyenma <miracleficient@gmail.com>
2021-02-25 10:47:11 +0100

    update: minor changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="about">
+  <section class="about hero">
     <div class="bg bg--page">
       <div class="bg__img-cont overlay overlay--main img-cont">
         <img src="@/assets/img/bg/a-pic-of-big-smallies.jpeg" alt="">
       </div>
     </div>
-    <h1>We can't wait to get in touch</h1>
-  </div>
+    <header class="about__hero hero">
+      <div class="hero__wrapper">
+        <h1 class="bg--txt bg--txt-bg hero__caption txt--h txt-5">We can't wait to get in touch</h1>
+      </div>
+    </header>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+  @import '../scss/_utils.scss';
+
+  .about{
+    &__hero{
+      justify-content: center;
+      text-align: center;
+
+      &.hero__wrapper{
+        width: 68%;
+      }
+    }
+
+  }
+</style>
