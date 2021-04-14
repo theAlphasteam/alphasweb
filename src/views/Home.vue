8cd70470beb7743b4d10181c5a856b99e44311ff
miracleonyenma <miracleficient@gmail.com>
2021-04-14 13:40:09 +0100

    update && feat: add project section - first draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,6 +36,52 @@
         </header>
       </div>
     </section>
+
+    <section class="projects site-sect pad--b-2em">
+      <div class="site-sect__wrapper max-width--1120">
+        <header class="projects__header mb--4">
+          <h1 class="heading txt txt--h txt-4">Projects</h1>
+          <p class="txt--p txt-1">
+            Here's what we've been up to
+          </p>
+        </header>
+        <div class="projects__gallery grid">
+          <article v-for="project in projects" :key="project.id" class="projects__item project glass glass--bg pad--2">
+            <header>
+              <h3 class="txt--h txt-1">
+                <a :href="project.link" v-if="project.link" target="_blank" rel="noopener noreferrer">
+                  <span>{{project.name}}</span>
+
+                  <span class="icon icon--sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-3 -3 24 24" width="24" height="24" preserveAspectRatio="xMinYMin" class="jam jam-link"><path d='M3.19 9.345a.97.97 0 0 1 1.37 0 .966.966 0 0 1 0 1.367l-2.055 2.052a1.932 1.932 0 0 0 0 2.735 1.94 1.94 0 0 0 2.74 0l4.794-4.787a.966.966 0 0 0 0-1.367.966.966 0 0 1 0-1.368.97.97 0 0 1 1.37 0 2.898 2.898 0 0 1 0 4.103l-4.795 4.787a3.879 3.879 0 0 1-5.48 0 3.864 3.864 0 0 1 0-5.47L3.19 9.344zm11.62-.69a.97.97 0 0 1-1.37 0 .966.966 0 0 1 0-1.367l2.055-2.052a1.932 1.932 0 0 0 0-2.735 1.94 1.94 0 0 0-2.74 0L7.962 7.288a.966.966 0 0 0 0 1.367.966.966 0 0 1 0 1.368.97.97 0 0 1-1.37 0 2.898 2.898 0 0 1 0-4.103l4.795-4.787a3.879 3.879 0 0 1 5.48 0 3.864 3.864 0 0 1 0 5.47L14.81 8.656z'/></svg>
+                  </span>
+                </a>
+                <span v-else>{{project.name}}</span>
+              </h3>
+              <p class="txt--p">{{project.description}}</p>
+
+              <div class="project__stats mt--1">
+                <span>{{project.collaborators.length}} collaborators</span>
+              </div>
+            </header>
+
+            <div class="project__action-cont">
+              <ul class="project__members">
+                <template v-for="(member, index) in project.collaborators" :key="member.id">
+                  <li class="project__member" v-if="index <= 1" v-bind:title="member.name">
+
+                  </li>
+                </template>
+
+                <span v-if="project.collaborators.length - 2 > 0">+{{project.collaborators.length - 2}} more</span>
+              </ul>
+
+              <button class="cta cta--main-gradient">View more</button>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -48,6 +94,49 @@ export default {
   components: {
     // HelloWorld,
   },
+  data(){
+    return{
+      projects: [
+        {
+          id: 1,
+          name: 'alphasweb',
+          link: 'http://alphasweb.netlify.app',
+          description: 'alphasweb official portfolio site',
+          collaborators: [
+            {
+              id: 1,
+              username: 'miracleio'
+            },
+            {
+              id: 2,
+              username: 'shori'
+            },
+            {
+              id: 3,
+              username: 'Divo'
+            }
+          ],
+          technologies: [
+            'vue', 'scss'
+          ]
+        },
+        {
+          id: 2,
+          name: 'Oshare',
+          description: 'Simple file sharing app built with VueJS',
+          collaborators: [
+            {
+              id: 1,
+              username: 'miracleio'
+            }
+          ],
+          technologies: [
+            'vue', 'scss'
+          ]
+        }
+      ]
+    }
+  }
 };
 </script>
 
@@ -102,4 +191,55 @@ export default {
     transform: translateY(-10%);
   }
 }
+
+.projects{
+  &__header{
+    // text-align: right;
+  }
+  &__gallery{
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: $defValEm * 2;
+  }
+}
+
+.project{
+  width: 100%;
+  min-height: 200px;
+  border-radius: $defValpx;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &__stats{
+    opacity: .4;
+  }
+
+  &__action-cont{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__members{
+    display: inline-flex;
+    align-items: center;
+
+    :nth-child(1){
+      left: 0;
+    }
+  }
+
+  &__member{
+    position: relative;
+    width: 28px;
+    height: 28px;
+    border-radius: 50%;
+    background: var(--main-gradient);
+    border: 1px solid var(--dark);
+
+    left: - $defValpx;
+  }
+}
+
 </style>
